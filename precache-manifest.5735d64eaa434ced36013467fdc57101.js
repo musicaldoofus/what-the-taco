36a77ccb407df5bf94459d6fc836c11ec6547264
musicaldoofus <burns.trainingdesign@gmail.com>
2019-07-25 17:08:05 -0700

    Updates

---
+++ precache-manifest.5735d64eaa434ced36013467fdc57101.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c0d5cf3f6861f7fc0b19ff2d7a4c780",
+    "url": "/what-the-taco/index.html"
+  },
+  {
+    "revision": "1545724b73153c5bd4ea",
+    "url": "/what-the-taco/static/css/main.4817af0e.chunk.css"
+  },
+  {
+    "revision": "4162ef01d2a50a263990",
+    "url": "/what-the-taco/static/js/2.7191ffc8.chunk.js"
+  },
+  {
+    "revision": "1545724b73153c5bd4ea",
+    "url": "/what-the-taco/static/js/main.5cccc0ab.chunk.js"
+  },
+  {
+    "revision": "4e75c4bf9df6bb36efd3",
+    "url": "/what-the-taco/static/js/runtime~main.aaeb460f.js"
+  },
+  {
+    "revision": "dd029ff3cc6c903010350dafaea3692b",
+    "url": "/what-the-taco/static/media/WhatTheTacoLogo.dd029ff3.svg"
+  },
+  {
+    "revision": "54f8769b3ccc4e11d46d6aae9834fcab",
+    "url": "/what-the-taco/static/media/WhatTheTacoLogoText.54f8769b.svg"
+  },
+  {
+    "revision": "595cd6ce569ecf10e543a967143c4305",
+    "url": "/what-the-taco/static/media/magnifying-glass.595cd6ce.svg"
+  },
+  {
+    "revision": "db6dc55ff72ba2c703b0d6318b541422",
+    "url": "/what-the-taco/static/media/stock-taco-1.db6dc55f.webp"
+  },
+  {
+    "revision": "9b4a95a7d7dccf8b623759b59acba815",
+    "url": "/what-the-taco/static/media/stock-taco-2.9b4a95a7.jpg"
+  }
+]);
